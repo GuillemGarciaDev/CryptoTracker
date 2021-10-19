baa622dc61b61273e89941773d48ab7496aa37df
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2021-10-19 20:10:42 +0200

    Added multiple crypto selects

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import toolbar as tl
 import os
 
+
+['bitcoin', 'ethereum', 'binancecoin', 'cardano', 'solana', 'ripple', 'polkadot', 'dogecoin', 'terra-luna']
+
+
 if __name__ == "__main__":
     os.system("rm -r sources")
     os.system("mkdir sources")
